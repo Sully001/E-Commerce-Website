f8b7ade34630c7c0cb26f00f5927ed30d7379e7b
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-10-25 12:37:08 +0100

    Added welcome and products pages to be worked on

---
+++ laravel-site/resources/views/products.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+<h1>This will be a products page</h1>
+<p>It will show all individual products on our page</p>
+<img src="https://www.hellotech.com/blog/wp-content/uploads/2020/02/what-is-a-gpu.jpg" alt="Image of a gpu" height="200px" width="200px">
+@endsection
