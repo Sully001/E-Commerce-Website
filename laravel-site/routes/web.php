7faf56d891fd1dcd6866d367cb4ed23d66c147b2
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-10-26 09:23:44 +0100

    Added welcome page file

--- laravel-site/routes/web.php
+++ laravel-site/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/products/{id}', function ($id) {
+    return view('products', ['product' => $id]);
+});
