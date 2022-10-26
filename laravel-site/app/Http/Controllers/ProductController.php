6b4a8db4fbcaef0382756095261bc26e7a5b3cbc
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-10-26 10:21:44 +0100

    Added product controller to get all products and get a single product

--- laravel-site/app/Http/Controllers/ProductController.php
+++ laravel-site/app/Http/Controllers/ProductController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+
+    //This method gets all products in the database and returns them
+    //to the views.
+    public function index() {
+        $products = Product::all();
+        return view('products', [
+            'products' => $products,
+        ]);
+    }
+
+    public function show($id) {
+        $product = Product::findorFail($id);
+        return view('/products/{id}', [
+            'product' => $product
+        ]);
+    }
 }
