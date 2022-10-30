6c9937d215a10d2c85b84e21e0fd3a085bbaf519
Samuel Elliot <210028353@aston.ac.uk>
2022-10-30 12:18:07 +0000

    Changed location for accounts information

---
+++ laravel-site/resources/php/accounts.php
@@ -0,0 +1,41 @@
+<?php
+class Account{
+    private $Name;
+    private $AccountType;  /* Testing Information */
+    private $Password;
+    private $Phone;
+    private $Email;
+    /* Need HTML form inputs to populate variables */
+
+    function __construct($Name, $AccountType, $Password, $Email, $Phone) {
+        $this->Name = $Name;
+        $this->AccountType = $AccountType;
+        $this->Password = $Password;
+        $this->Email = $Email;
+        $this->Phone = $Phone;
+    }
+
+    function getName() {
+ 
+    }
+
+    function getAccountType() {
+
+    }
+
+    function getPhone() {
+
+    }
+
+    function getEmail() {
+        
+    }
+
+}
+
+
+class AccountCreate {
+
+}
+
+?>
