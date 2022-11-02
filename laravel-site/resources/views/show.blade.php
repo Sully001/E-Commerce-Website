bb07ca62090cdd79542c4f8b4bfa4c348f23c003
Jenson <jensonjamesross17@gmail.com>
2022-11-02 14:40:00 +0000

    updated product page to put header and body
    Head and body added

--- laravel-site/resources/views/show.blade.php
+++ laravel-site/resources/views/show.blade.php
@@ -3,7 +3,10 @@
 
 @section('content')
 <div class = "product">
+<header>
 <h1>$product-ProductName></h1>
+</header>
+<body>
 <img src="{{ $product-> }}" alt="Product Image" height="200px" width="200px">
 <h4><strong>Price: {{ $product->Price }}</strong></h4>
 
@@ -15,5 +18,6 @@
 
 <h4>Description</h4>
 <p>{{ $product->Description }}</p>
+</body>
 </div>
 @endsection
