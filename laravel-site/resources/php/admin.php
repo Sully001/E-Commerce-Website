dbeb3dee96721cb6feade57706236de9470436d5
Samuel Elliot <210028353@aston.ac.uk>
2022-11-02 10:12:03 +0000

    Added admin page

---
+++ laravel-site/resources/php/admin.php
@@ -0,0 +1,7 @@
+<?php
+/*
+*This page allows admins to check stock, list of customers, and current orders.
+*A check needs to be preformed, to make sure the user is an admin.
+*They can then access admin controls.
+*/
+?>
