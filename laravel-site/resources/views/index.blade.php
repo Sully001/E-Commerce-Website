9af17ecf614e6f77cecc5a40a17163b03a9ce99f
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-02 12:09:54 +0000

    Added some styling for products page

--- laravel-site/resources/views/index.blade.php
+++ laravel-site/resources/views/index.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="all-products">
-@foreach ($products as $product)
-    <a href="/products/{{ $product->product_id }}">
-        <div>
-            <img src="{{ $product->product_image}}" alt="This products image" height="200px" width="200px">
-            <p><strong>{{ $product->name }}</strong></p>
-            <p>£{{ $product->price}}</p>
-            
-            @if ($product->quantity > 0)
-                <p>In Stock</p>
-            @else
-                <p>Out of Stock</p>
-            @endif
-        </div>
-    </a>
-@endforeach
-</div>
+
+    <h1 class="all-products-title">All Products</h1>
+
+
+    <div class="all-products">
+    @foreach ($products as $product)
+        <a href="/products/{{ $product->product_id }}">
+            <div>
+                <img src="{{ $product->product_image}}" alt="This products image" height="200px" width="200px">
+                <p><strong>{{ $product->name }}</strong></p>
+                <p>£{{ $product->price}}</p>
+                
+                @if ($product->quantity > 0)
+                    <p>In Stock</p>
+                @else
+                    <p>Out of Stock</p>
+                @endif
+            </div>
+        </a>
+    @endforeach
+    </div>
 @endsection
