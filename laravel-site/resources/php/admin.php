0dc29898ff2c8bd0e67c0235dc69022822fb7450
Samuel Elliot <210028353@aston.ac.uk>
2022-11-02 10:37:17 +0000

    Admin skeleton created.

--- laravel-site/resources/php/admin.php
+++ laravel-site/resources/php/admin.php
@@ -4,4 +4,24 @@
 *A check needs to be preformed, to make sure the user is an admin.
 *They can then access admin controls.
 */
+session_start();
+if(!isset($_SESSION['username'])) { #If no username is set, refuse assess.
+    header("Location:landingpage.php"); #Send user back home.
+    exit();
+}
+#Fetch account type by the username.
+if($_session['username']/*...*/) { #Checks the user is an admin, if not run this
+    echo '<script>popup.js</script>';
+    header("Location:landingpage.php");
+    session_destroy(); #End session after trying to access admin account.
+    exit();
+}
+#If all checks complete, move onto admin information:
 ?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Administrator Controls</title>
+    </head>
+</html>
