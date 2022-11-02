443cae41c84bdbb3fed3e782c75e42006147c7b2
Jenson <jensonjamesross17@gmail.com>
2022-11-02 14:08:30 +0000

    Made the product page so CSS can align it all

--- laravel-site/resources/views/show.blade.php
+++ laravel-site/resources/views/show.blade.php
@@ -2,18 +2,18 @@
 
 
 @section('content')
-<h1>This will show a single product on the page</h1>
-<img src="{{ $product->product_image }}" alt="Product Image" height="200px" width="200px">
-<h3>{{ $product->name}}</h3>
-<p><strong>Price - {{ $product->price }}</strong></p>
+<div class = "product">
+<h1>$product-ProductName></h1>
+<img src="{{ $product-> }}" alt="Product Image" height="200px" width="200px">
+<h4><strong>Price: {{ $product->Price }}</strong></h4>
 
-@if ($product->quantity > 0)
-    <p>In Stock</p>
+@if ($product->Quantity > 0)
+    <h4>In Stock</h4>
     @else
-    <p>Out of Stock</p>
+    <h4>Out of Stock</h4>
 @endif
 
-<h3>Description</h3>
-<p>{{ $product->description }}</p>
-
+<h4>Description</h4>
+<p>{{ $product->Description }}</p>
+</div>
 @endsection
