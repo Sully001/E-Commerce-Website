952818b4572438f72faeace0463c51454edbfe14
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-02 12:04:18 +0000

    Added contact and about pages and added navbar with some styling

---
+++ laravel-site/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.layout')
+
+@section('content')
+<h1>This is the about page</h1>
+
+@endsection
---
+++ laravel-site/resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+<h1>This is the contact page</h1>
+
+
+@endsection
