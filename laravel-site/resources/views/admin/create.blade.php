af25819b21eb8f2c48e1955d01937f35c2462ca5
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-02 10:29:52 +0000

    Added admin dashboard and create products page route

---
+++ laravel-site/resources/views/admin/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+<h1>This is the create form for the admin</h1>
+
+<form action=""></form>
+@endsection
---
+++ laravel-site/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('content')
+<h1>This is the dashboard for the admin</h1>
+@endsection
