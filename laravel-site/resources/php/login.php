7e2519935ad2d60ff90cb7bb8405f948faae3801
Samuel Elliot <210028353@aston.ac.uk>
2022-11-02 10:05:31 +0000

    Finished login implementation

--- laravel-site/resources/php/login.php
+++ laravel-site/resources/php/login.php
@@ -4,4 +4,32 @@
 *Requires a HTML form, with username and password.
 *Uses sessions.
 */
+
+if(isset($_POST['submitted'])) { #If the submission button is clicked, run this PHP
+    if(!isset($_POST['username'], $_POST['password'])) { #Checks fields in HTML form are filled in (Validation!).
+        exit("Please enter a username and password.");
+    }
+    require_once("connect.php"); #Connects to the database through the connect.php file.
+    try {
+        $status = $db->prepare('SELECT password FROM databasename WHERE email = ?'); #Protects from SQLInjection, preparing SQL query. 
+        $status->execute(array($_POST['username'])); #Running the query.
+        if($status->rowCount()>0) { #Check username exists.
+            $row = $status->fetch(); #Get username
+            if(password_verify($_POST['password'], $row['password'])) { #Checks passwords match.
+                session_start(); #Starts session to keep user logged in.
+                $_SESSION['username']=$_POST['username'];
+                header("Location:landingpage.html"); #Redirects user to landing page, logged in.
+                exit();
+            } else {
+                echo "Error, password doesn't match"; #Displays error if password donesn't match.
+            }
+        } else {
+            echo "No username found. Please check username."; #Displays errors if username isn't found.
+        }
+
+    } catch (PDOException $e) {
+        echo "An error occured when trying to login. Please try again."; #If a error occours, like database connection, display error.
+        echo $e;
+    }
+}
 ?>
