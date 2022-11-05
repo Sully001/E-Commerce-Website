082c4f307c2351472851026a2fa1d8b04e576e90
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-05 19:06:29 +0000

    Added html for admin to add product

--- laravel-site/resources/views/admin/create.blade.php
+++ laravel-site/resources/views/admin/create.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.layout')
 
 @section('content')
-<h1>This is the create form for the admin</h1>
+<h1>Add a New Product</h1>
 
-<form action=""></form>
+<form action="/admin/dashboard" method="POST" enctype="multipart/form-data">
+    @csrf
+    <label for="product_name">Product Name: </label>
+    <input type="text" name="productname"> <br><br>
+
+    <label for="price">Price</label>
+    <input type="number" name="price"> <br><br>
+
+    <label for="qty">Quantity</label>
+    <input type="number" name="qty" min="1" max="100"> <br><br>
+
+    <label for="description">Description</label>
+    <input type="text" name="description"> <br><br>
+
+    <label for="category">Category</label>
+    <select name="categories" id="categories">
+        <option value="GPU">GPU</option>
+        <option value="CPU">CPU</option>
+        <option value="RAM">RAM</option>
+        <option value="motherboards">Motherboards</option>
+        <option value="storage">Storage</option>
+        <option value="monitors">Monitors</option>
+        <option value="psu">PSU</option> 
+    </select><br><br>
+
+    <label for="imageurl">Image URL</label>
+    <input type="file" name="image" id="image"> <br><br>
+    <input type="submit" value="Add Product">
+</form>
 @endsection
