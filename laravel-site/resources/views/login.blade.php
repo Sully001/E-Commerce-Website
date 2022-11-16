337c8d861250336bc81b4b083f5b679d1cc51df1
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-16 20:35:16 +0000

    html added for login and signup pages

--- laravel-site/resources/views/login.blade.php
+++ laravel-site/resources/views/login.blade.php
@@ -5,7 +5,27 @@
 @endsection
 
 @section('content')
-<h1>This is the login page</h1>
 
-<form action=""></form>
+<div class="container-md">
+    <div class="text-center">
+        <h2>Welcome To Hardware 28 Online Store</h2>
+        <p class="lead">Sign In to Shop For All The Latest Products</p>
+    </div>
+    
+    <div class="row justify-content-center my-3">
+        <div class="col-lg-5 col-md-8">
+            <form action="" method="POST">
+                @csrf
+                <label for="email" class="form-label mt-2">Email</label>
+                <input type="text" id="email" name="email" class="form-control" placeholder="Email" required>
+
+                <label for="password" class="form-label mt-2">Password</label>
+                <input type="text" id="password" name="password" class="form-control" placeholder="Password" required>
+                
+                    <button type="submit" class="btn btn-primary mt-2 text-center">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
--- laravel-site/resources/views/signup.blade.php
+++ laravel-site/resources/views/signup.blade.php
@@ -6,7 +6,29 @@
 
 
 @section('content')
-<h1>This is the signup page</h1>
+<div class="container-md">
+    <div class="text-center">
+        <h2>Welcome To Hardware 28 Online Store</h2>
+        <p class="lead">Sign Up To Enjoy Our Site</p>
+    </div>
+    
+    <div class="row justify-content-center my-3">
+        <div class="col-lg-5 col-md-8">
+            <form action="" method="POST">
+                @csrf
+                <label for="name" class="form-label mt-2">Email</label>
+                <input type="text" id="name" name="name" class="form-control" placeholder="Name" required>
 
-<form action=""></form>
+                <label for="email" class="form-label mt-2">Email</label>
+                <input type="text" id="email" name="email" class="form-control" placeholder="Email" required>
+
+                <label for="password" class="form-label mt-2">Password</label>
+                <input type="text" id="password" name="password" class="form-control" placeholder="Password" required>
+                
+                <button type="submit" class="btn btn-primary mt-2 text-center">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
