38aa358483a524fea35c5f8e5d90c4ae19023f73
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-18 21:22:10 +0000

    Added google font styling for navbar

--- laravel-site/resources/views/layouts/layout.blade.php
+++ laravel-site/resources/views/layouts/layout.blade.php
@@ -7,6 +7,9 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" href="/css/main.css" type="text/css">
