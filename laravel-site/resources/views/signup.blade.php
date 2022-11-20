f57f6f1b1ba69700b2432dd01f62002aebd74d32
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-20 13:47:08 +0000

    Made changes to sign up form

--- laravel-site/resources/views/signup.blade.php
+++ laravel-site/resources/views/signup.blade.php
@@ -17,13 +17,16 @@
             <form action="" method="POST">
                 @csrf
                 <label for="name" class="form-label mt-2">Name</label>
-                <input type="text" id="name" name="name" class="form-control" placeholder="Name" required>
+                <input type="text" id="name" name="name" class="form-control" placeholder="Enter name" required>
 
                 <label for="email" class="form-label mt-2">Email</label>
-                <input type="email" id="email" name="email" class="form-control" placeholder="Email" required>
+                <input type="email" id="email" name="email" class="form-control" placeholder="Enter email" required>
 
                 <label for="password" class="form-label mt-2">Password</label>
-                <input type="text" id="password" name="password" class="form-control" placeholder="Password" required>
+                <input type="password" id="password" name="password" class="form-control" placeholder="Enter password" required>
+
+                <label for="password_confirmation" class="form-label mt-2">Retype Password</label>
+                <input type="password" id="password" name="password_confirmation" class="form-control" placeholder="Retype your password" required>
                 
                 <button type="submit" class="btn btn-primary mt-2 text-center">Submit</button>
                 </div>
