fc39c9d3bac9de8cb6cace6e595671a04c3d7af4
Ahsaan Kumar <200070104@aston.ac.uk>
2022-11-21 18:26:25 +0000

    Made the About Us & Contact Us

--- laravel-site/resources/views/about.blade.php
+++ laravel-site/resources/views/about.blade.php
@@ -5,5 +5,12 @@
 @endsection
 
 @section('content')
-<h1>About to do the page</h1>
+<div class="centerText">
+<h1>About Us</h1>
+</div>
+<div class="textSplit">
+    <h4>We are a small ecommerce business from Aston, Birmingham. Our vision at Hardware 28 is to ensure that we deliver high quality computer hardware. </h4>
+</div>
+<div class="textSplit">
+        <h4>At hardware 28, we are big gamers that aspire to have a competitive advantage whilst gaming so having the correct hardware to allow us to do so </h4><h4>was always our priority.  It's now our priority to ensure that our customers can have the same experience hence why we started this business.</h4>
 @endsection
--- laravel-site/resources/views/contact.blade.php
+++ laravel-site/resources/views/contact.blade.php
@@ -5,6 +5,34 @@
 @endsection
 
 @section('content')
-<h3>This is the contact page</h3>
-<p>asfjjik</p>
+<div class="ContactUs">
+<h1>Contact Us</h1>
+</div>
+<div class="furtherquestions">
+<p>Have any further questions? feel free to contact us!</p>
+</div>
+
+<div class="splitText">
+</div>
+
+<a href="https://outlook.live.com/owa/">
+<img class="E-Mail" src="/images/outlook-favicon.png" alt="E-Mail Image">
+
+
+<a href="https://github.com/">
+<img class="Github" src="/images/github-favicon.png" alt="Github Image">
+
+<a href="https://www.aston.ac.uk/">
+<img class="Address" src="/images/location-favicon.png" alt="Address">
+<div class="p">
+    
+</a>
+    
+<h6>200070104@aston.ac.uk
+    <div class="h6a">    
+    Github
+    <div class="h6b">  
+    Aston St, Birmingham B4 7ET
+</h6>
+</div></div>      
 @endsection
