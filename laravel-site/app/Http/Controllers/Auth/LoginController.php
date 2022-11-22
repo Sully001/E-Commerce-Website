6725dc9ca27fddb13318b8111602db3091664ee2
Samuel Elliot <210028353@aston.ac.uk>
2022-11-22 19:17:19 +0000

    Added Logging controller

---
+++ laravel-site/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function store(Request $request)
+    {
+        $userCredentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        
+        if(Auth::attempt($userCredentials)) {
+            $request->session()->regenerate();
+            //HERE IDENTIFY IF USER IS AN ADMIN OR NOT
+            if(Auth::attempt(['admin'=> "Admin"])) {
+                return redirect()->intended('admin.dashboard');
+            }
+            return redirect()->intended('welcome');
+        }
+
+        return back()->withErrors([
+            'email' => 'Your E-Mail or password is incorrect. Please try again.',
+        ])->onlyInput('email');
+    }
+}
