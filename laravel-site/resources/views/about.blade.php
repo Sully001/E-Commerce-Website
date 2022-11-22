7583bff9922b9b417a413938dadff6881ac89f10
Ahsaan Kumar <200070104@aston.ac.uk>
2022-11-22 13:33:59 +0000

    Updated About Us & Contact Us

--- laravel-site/resources/views/about.blade.php
+++ laravel-site/resources/views/about.blade.php
@@ -12,5 +12,13 @@
     <h4>We are a small ecommerce business from Aston, Birmingham. Our vision at Hardware 28 is to ensure that we deliver high quality computer hardware. </h4>
 </div>
 <div class="textSplit">
-        <h4>At hardware 28, we are big gamers that aspire to have a competitive advantage whilst gaming so having the correct hardware to allow us to do so </h4><h4>was always our priority.  It's now our priority to ensure that our customers can have the same experience hence why we started this business.</h4>
+        <h4>At hardware 28, we are big gamers that aspire to have a competitive advantage whilst gaming so having the correct hardware to allow us to do so was always our priority.  It's now our priority to ensure that our customers can have the same experience hence why we started this business.</h4>
+</div>
+<!-- <div class="imageContainer">
+<div class="left"> -->
+<img class="AboutUsPics" src="/images/aboutuspic.jpeg" alt="Campus" >  
+<!-- </div> -->
+<!-- <div class="right">      
+<img class="AboutUsPics" src="/images/Colossus.jpg" alt="Colossus"height="800px" width="800px">  
+</div> -->
 @endsection
