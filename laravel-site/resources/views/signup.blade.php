2c9322c678dd48582ac93fc16cd3caba596e1801
Samuel Elliot <210028353@aston.ac.uk>
2022-11-22 11:17:00 +0000

    Added

--- laravel-site/resources/views/signup.blade.php
+++ laravel-site/resources/views/signup.blade.php
@@ -14,7 +14,7 @@
     
     <div class="row justify-content-center my-3">
         <div class="col-lg-5 col-md-8">
-            <form action="" method="POST">
+            <form action="/register" method="POST">
                 @csrf
                 <label for="name" class="form-label mt-2">Name</label>
                 <input type="text" id="name" name="name" class="form-control" placeholder="Enter name" required>
@@ -22,7 +22,7 @@
                 <label for="email" class="form-label mt-2">Email</label>
                 <input type="email" id="email" name="email" class="form-control" placeholder="Enter email" required>
 
-                <label for="password" class="form-label mt-2">Password</label>
+                <label for="password_confirmation" class="form-label mt-2">Password</label>
                 <input type="password" id="password" name="password" class="form-control" placeholder="Enter password" required>
 
                 <label for="password_confirmation" class="form-label mt-2">Retype Password</label>
--- laravel-site/routes/web.php
+++ laravel-site/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -73,3 +75,10 @@ Route::get('/contact', function() {
 Route::get('/basket', function() {
     return view('basket');
 });
+
+//Route for register page.
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
