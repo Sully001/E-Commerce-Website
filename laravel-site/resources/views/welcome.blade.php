700e8dbc93dd24aa7259e9a4e2fe2d39389b31f7
Ahsaan Kumar <200070104@aston.ac.uk>
2022-11-23 10:56:58 +0000

    Made and Designed Welcome Page

--- laravel-site/resources/views/welcome.blade.php
+++ laravel-site/resources/views/welcome.blade.php
@@ -1,10 +1,22 @@
 @extends('layouts.layout')
 
 @section('css')
-    <link rel="stylesheet" href="" type="text/css">
+    <link rel="stylesheet" href="/css/welcome.css" type="text/css">
 @endsection
 
 @section('content')
-<h1>Welcome - {{ Session::get('name') }} - {{ Session::get('id') }}</h1>
+<div class="Welcome">
+<h1>Welcome to Hardware28</h1>
+</div>
+
+
+
+
+
+<!-- <div class="ViewProducts">
+    <p>Click the button to View our Products!</p>
+</div> -->
+<div class="product">
+<a href="products"><productRedirect>View our Products</productRedirect></a>
+</div>  
 @endsection
-    
