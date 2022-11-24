08b4856954f53ca204e7af28b28c2a7672d9c521
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-24 17:23:53 +0000

    Added order controller, model and migration

---
+++ laravel-site/database/migrations/2022_11_24_170014_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->integer('orderID');
+            $table->integer('userID');
+            $table->integer('productID');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
