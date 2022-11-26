c9d8b11376ed285eb1ab587d520944a7c35e1a6d
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2022-11-26 23:54:36 +0000

    Added some small improvements to the css and html

--- laravel-site/resources/views/previousorders.blade.php
+++ laravel-site/resources/views/previousorders.blade.php
@@ -8,6 +8,7 @@
 <div class="text-center">
     <h2>User {{ Session::get('id') }}'s Previous Orders</h2>
     <p class="lead">Open Each Order and Click View Details To See More</p>
+    <p class="lead">You Latest Orders Are At The Top!</p>
 </div>
 @for ($i = count($uniqueOrderID); $i > 0; $i--)
     <div id="container">
@@ -15,20 +16,21 @@
             <div class="accordion-item">
                 <h2 class="accordion-header" id="heading{{$i}}">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$i}}" aria-expanded="true" aria-controls="collapse{{$i}}">
-                    Order {{$i}}
+                    <strong>Order {{$i}}</strong> 
                 </button>
                 </h2>
                 <div id="collapse{{$i}}" class="accordion-collapse collapse" aria-labelledby="heading{{$i}}" data-bs-parent="#accordionExample">
-                <div class="accordion-body" id="accordion-body">
+                <div class="accordion-body bg-secondary" id="accordion-body">
                     <div>
                         @for($x = count($orders) - 1; $x >= 0; $x--)
                             @if ($orders[$x]['orderID'] == $uniqueOrderID[($i - 1)]['orderID'])
-                                <p>Product ID - {{$orders[$x]['productID']}} (X {{ $orders[$x]['quantity']}})</p>
+                                <p><strong>Product ID</strong>
+                             - {{$orders[$x]['productID']}} <br>(<strong>Quantity</strong>: X {{ $orders[$x]['quantity']}})</p>
                             @endif
                         @endfor
                     </div>
                     <div class="subtotal">
-                        <p>SubTotal - £2000</p>
+                        <p><strong>SubTotal - £2000</strong></p>
                     </div>
                     <div class="button-div">
                         <form action="{{route('previous.details', $uniqueOrderID[($i-1)]['orderID'])}}">
