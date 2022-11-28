8f0ac9ad60157497ddf0cd0d73d462705babba0e
Samuel Elliot <210028353@aston.ac.uk>
2022-11-28 20:54:18 +0000

    Finished AdminAccess Middleware

--- laravel-site/app/Http/Middleware/AdminAccess.php
+++ laravel-site/app/Http/Middleware/AdminAccess.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAccess
 {
@@ -16,6 +17,12 @@ class AdminAccess
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+
+        if(Auth::user() && Auth::user()->admin == "Admin") {
+            return $next($request);
+        }
+        
+        return redirect('/')->with('bounds','You dont have permission for this area');
+
     }
 }
