b38a1f87fca68b92a0fc85504c3e064bc0ab4d46
Ahsaan Kumar <200070104@aston.ac.uk>
2022-11-30 13:07:37 +0000

    fixed styling again

--- laravel-site/resources/views/welcome.blade.php
+++ laravel-site/resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
     <div class="product">
       <h1>Welcome to Hardware28</h1>
-      <a href="products" style="text-decoration: none;"><productRedirect>View our Products</productRedirect></a>
+      <a href="products" style="text-decoration: none;"><div class="product"><productRedirect>View our Products</productRedirect></product></a>
   </div>  
 </div>
 @endsection
